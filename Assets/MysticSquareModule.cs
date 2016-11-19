b383b91abe7213866a40d9126084df2ee71dcbe9
Timwi <github@timwi.de>
2016-11-19 12:58:36 +0100

    Finish code clean-up, manual page, fix display of skull/knight, fix appearance of highlight.

--- Assets/MysticSquareModule.cs
+++ Assets/MysticSquareModule.cs
@@ -8,15 +8,16 @@ using Rnd = UnityEngine.Random;
 
 public class MysticSquareModule : MonoBehaviour
 {
-    public KMSelectable[] buttons;
-    public Transform[] visibleButtons;
-    public Transform emptyField;
+    public KMSelectable[] ButtonSelectables;
+    public Transform[] ButtonObjects;
+    public Transform EmptyField, Skull, Knight;
 
-    int[] field = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
-    int skull, knight, winCond;
-    bool inDanger;
+    int[] _field = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+    int _skullPos, _knightPos, _winningCondition;
+    bool _isInDanger;
+    bool _isActivated;
 
-    static int[,] table = new int[8, 8] {
+    static int[,] _table = new int[8, 8] {
         { 1, 3, 5, 4, 6, 7, 2, 8 },
         { 2, 5, 7, 3, 8, 1, 4, 6 },
         { 6, 4, 8, 1, 7, 3, 5, 2 },
@@ -29,39 +30,39 @@ public class MysticSquareModule : MonoBehaviour
 
     void Start()
     {
-        for (int i = 0; i < buttons.Length; i++)
+        for (int i = 0; i < ButtonSelectables.Length; i++)
         {
             int j = i;
-            if (buttons[i].gameObject.name != "empty")
-                buttons[i].OnInteract += delegate { OnPress(j); return false; };
+            ButtonSelectables[i].OnInteract += delegate { OnPress(j); return false; };
         }
 
         GetComponent<KMBombModule>().OnActivate += OnActivate;
 
-        inDanger = true;
-        skull = 0;
-        knight = 0;
+        _isInDanger = true;
+        _isActivated = false;
+        _skullPos = 0;
+        _knightPos = 0;
 
         initArray();
 
         // Find winning condition
-        winCond = 0;
+        _winningCondition = 0;
         if (topSum() > horMidSum() && topSum() > bottomSum())
-            winCond += 0;
+            _winningCondition += 0;
         else if (horMidSum() > topSum() && horMidSum() > bottomSum())
-            winCond += 10;
+            _winningCondition += 10;
         else if (bottomSum() > topSum() && bottomSum() > horMidSum())
-            winCond += 20;
+            _winningCondition += 20;
         else
-            winCond += 30;
+            _winningCondition += 30;
         if (leftSum() > verMidSum() && leftSum() > rightSum())
-            winCond += 0;
+            _winningCondition += 0;
         else if (verMidSum() > leftSum() && verMidSum() > rightSum())
-            winCond += 1;
+            _winningCondition += 1;
         else if (rightSum() > leftSum() && rightSum() > verMidSum())
-            winCond += 2;
+            _winningCondition += 2;
         else
-            winCond += 3;
+            _winningCondition += 3;
 
         // Set start field
         int[] start = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
@@ -71,28 +72,28 @@ public class MysticSquareModule : MonoBehaviour
         {
             for (int j = 0; j <= 8; j++)
             {
-                if (start[j] == field[i])
+                if (start[j] == _field[i])
                 {
                     if (start[j] == 0)
                     {
                         if (start[i] != 0)
                         {
-                            dif = visibleButtons[start[i] - 1].gameObject.transform.position - emptyField.transform.position;
-                            visibleButtons[start[i] - 1].gameObject.transform.Translate(-dif, Space.World);
-                            emptyField.transform.Translate(dif, Space.World);
+                            dif = ButtonObjects[start[i] - 1].gameObject.transform.position - EmptyField.transform.position;
+                            ButtonObjects[start[i] - 1].gameObject.transform.Translate(-dif, Space.World);
+                            EmptyField.transform.Translate(dif, Space.World);
                         }
                     }
                     else if (start[i] != 0)
                     {
-                        dif = visibleButtons[start[i] - 1].gameObject.transform.position - visibleButtons[start[j] - 1].gameObject.transform.position;
-                        visibleButtons[start[i] - 1].gameObject.transform.Translate(-dif, Space.World);
-                        visibleButtons[start[j] - 1].gameObject.transform.Translate(dif, Space.World);
+                        dif = ButtonObjects[start[i] - 1].gameObject.transform.position - ButtonObjects[start[j] - 1].gameObject.transform.position;
+                        ButtonObjects[start[i] - 1].gameObject.transform.Translate(-dif, Space.World);
+                        ButtonObjects[start[j] - 1].gameObject.transform.Translate(dif, Space.World);
                     }
                     else
                     {
-                        dif = emptyField.transform.position - visibleButtons[start[j] - 1].gameObject.transform.position;
-                        emptyField.transform.Translate(-dif, Space.World);
-                        visibleButtons[start[j] - 1].gameObject.transform.Translate(dif, Space.World);
+                        dif = EmptyField.transform.position - ButtonObjects[start[j] - 1].gameObject.transform.position;
+                        EmptyField.transform.Translate(-dif, Space.World);
+                        ButtonObjects[start[j] - 1].gameObject.transform.Translate(dif, Space.World);
                     }
 
                     var temp = start[j];
@@ -105,6 +106,7 @@ public class MysticSquareModule : MonoBehaviour
 
     void OnActivate()
     {
+        _isActivated = true;
         string serial;
         List<string> serialResponses = GetComponent<KMBombInfo>().QueryWidgets(KMBombInfo.QUERYKEY_GET_SERIAL_NUMBER, null);
         if (serialResponses.Count == 0)
@@ -117,235 +119,147 @@ public class MysticSquareModule : MonoBehaviour
             Dictionary<string, string> serialDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(serialResponses[0]);
             serial = serialDict["serial"];
         }
-        var sNumber = serial[5] - 48;
-        Debug.Log("Serial: " + serial + "          sNumber: " + sNumber);
+        var lastSerialDigit = serial[5] - 48;
+        Debug.Log("Serial: " + serial + "          lastSerialDigit: " + lastSerialDigit);
 
         // Place objectives
+        var useRows = lastSerialDigit != 0 && (lastSerialDigit == _field[0] || lastSerialDigit == _field[2] || lastSerialDigit == _field[4] || lastSerialDigit == _field[6] || lastSerialDigit == _field[8]);
 
         // If midfield is empty, skull is under the 7
-        if (field[4] == 0)
+        if (_field[4] == 0)
         {
-            skull = Array.IndexOf(field, 7);
+            _skullPos = Array.IndexOf(_field, 7);
         }
         else
         {
-            int checkN;
             for (int k = 0; k <= 7; k++)
             {
-                if (sNumber != 0 && (sNumber == field[0] || sNumber == field[2] || sNumber == field[4] || sNumber == field[6] || sNumber == field[8]))
-                    checkN = table[(field[4] == 0) ? 1 : (field[4] - 1), k];        // if midfield is empty
-                else
-                    checkN = table[k, (field[4] == 0) ? 1 : (field[4] - 1)];
-
-                Debug.Log("checkN: " + checkN);
-                for (int i = 0; i <= 8; i++)
-                {
-                    if (field[i] == checkN)
-                    {
-                        int verDif = i / 3 - skull / 3;
-                        int horDif = i % 3 - skull % 3;
-                        if ((Mathf.Abs(horDif) + Mathf.Abs(verDif)) == 1)
-                        {
-                            skull = i;
-                            Debug.Log("skull under: " + field[i]);
-                        }
-                        break;
-                    }
-                }
+                var checkN = useRows ? _table[_field[4] - 1, k] : _table[k, _field[4] - 1];
+                var ix = Array.IndexOf(_field, checkN);
+                int verDif = ix / 3 - _skullPos / 3;
+                int horDif = ix % 3 - _skullPos % 3;
+                if ((Mathf.Abs(horDif) + Mathf.Abs(verDif)) == 1)
+                    _skullPos = ix;
             }
         }
+        Debug.Log("Skull under: " + _field[_skullPos]);
 
-        knight = skull;
-        while (knight == skull || field[knight] == 0)
-            knight = Rnd.Range(0, 9);
+        do
+            _knightPos = Rnd.Range(0, 9);
+        while (_knightPos == _skullPos || _field[_knightPos] == 0);
 
-        var background = GetComponentsInChildren<Renderer>().FirstOrDefault(r => r.gameObject.name == "RecessedInner");
-
-        //skull texture positioning
-        int zOffset = 0;
-        int xOffset = 0;
-        if (skull < 3)
-            zOffset = 2;
-        else if (skull < 6)
-            zOffset = 1;
-        if (skull == 1 || skull == 4 || skull == 7)
-            xOffset = 1;
-        else if (skull == 2 || skull == 5 || skull == 8)
-            xOffset = 2;
-        background.materials[2].mainTextureOffset = new Vector2(-1.65f - xOffset * 0.85f, -2f - zOffset * 0.95f);
-
-        //knight texture positioning
-        zOffset = 0;
-        xOffset = 0;
-        if (knight < 3)
-            zOffset = 2;
-        else if (knight < 6)
-            zOffset = 1;
-        if (knight == 1 || knight == 4 || knight == 7)
-            xOffset = 1;
-        else if (knight == 2 || knight == 5 || knight == 8)
-            xOffset = 2;
-        background.materials[1].mainTextureOffset = new Vector2(-1.65f - xOffset * 0.85f, -2f - zOffset * 0.95f);
+        Skull.localPosition = new Vector3(.063f - (_skullPos % 3) * .045f, 0, -.028f + .0425f * (_skullPos / 3));
+        Knight.localPosition = new Vector3(.063f - (_knightPos % 3) * .045f, 0, -.028f + .0425f * (_knightPos / 3));
     }
 
     int topSum()
     {
-        return field[0] + field[1] + field[2];
+        return _field[0] + _field[1] + _field[2];
     }
 
     int horMidSum()
     {
-        return field[3] + field[4] + field[5];
+        return _field[3] + _field[4] + _field[5];
     }
 
     int bottomSum()
     {
-        return field[6] + field[7] + field[8];
+        return _field[6] + _field[7] + _field[8];
     }
 
     int leftSum()
     {
-        return field[0] + field[3] + field[6];
+        return _field[0] + _field[3] + _field[6];
     }
 
     int verMidSum()
     {
-        return field[1] + field[4] + field[7];
+        return _field[1] + _field[4] + _field[7];
     }
 
     int rightSum()
     {
-        return field[2] + field[5] + field[8];
+        return _field[2] + _field[5] + _field[8];
     }
 
     bool checkWin()
     {
-        if (field.SequenceEqual(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 0 }))
+        if (_field.SequenceEqual(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 0 }))
             return true;
 
-        switch (winCond)
+        switch (_winningCondition)
         {
-            case 0: return field[0] == 1 && field[2] == 2 && field[6] == 4 && field[8] == 3;
-            case 1: return field[0] == 1 && field[2] == 2 && field[6] == 3 && field[8] == 4;
-            case 2: return field[0] == 1 && field[2] == 3 && field[6] == 7 && field[8] == 5;
-            case 3: return field[0] == 1 && field[2] == 3 && field[6] == 5 && field[8] == 7;
-            case 10: return field[1] == 1 && field[5] == 2 && field[7] == 3 && field[3] == 4;
-            case 11: return field[1] == 1 && field[5] == 2 && field[7] == 4 && field[3] == 3;
-            case 12: return field[1] == 2 && field[5] == 4 && field[7] == 6 && field[3] == 8;
-            case 13: return field[1] == 2 && field[5] == 4 && field[7] == 8 && field[3] == 6;
-            case 20: return field[0] == 1 && field[4] == 2 && field[8] == 3;
-            case 21: return field[6] == 1 && field[4] == 2 && field[2] == 3;
-            case 22: return field[0] == 3 && field[4] == 2 && field[8] == 1;
-            case 23: return field[6] == 3 && field[4] == 2 && field[2] == 1;
-            case 30: return field[0] == 1 && field[1] == 2 && field[2] == 3 && field[4] == 4 && field[7] == 5;
-            case 31: return field[0] == 1 && field[3] == 2 && field[6] == 3 && field[4] == 4 && field[5] == 5;
-            case 32: return field[6] == 1 && field[7] == 2 && field[8] == 3 && field[4] == 4 && field[1] == 5;
-            case 33: return field[2] == 1 && field[5] == 2 && field[8] == 3 && field[4] == 4 && field[3] == 5;
+            case 0: return _field[0] == 1 && _field[2] == 2 && _field[6] == 4 && _field[8] == 3;
+            case 1: return _field[0] == 1 && _field[2] == 2 && _field[6] == 3 && _field[8] == 4;
+            case 2: return _field[0] == 1 && _field[2] == 3 && _field[6] == 7 && _field[8] == 5;
+            case 3: return _field[0] == 1 && _field[2] == 3 && _field[6] == 5 && _field[8] == 7;
+            case 10: return _field[1] == 1 && _field[5] == 2 && _field[7] == 3 && _field[3] == 4;
+            case 11: return _field[1] == 1 && _field[5] == 2 && _field[7] == 4 && _field[3] == 3;
+            case 12: return _field[1] == 2 && _field[5] == 4 && _field[7] == 6 && _field[3] == 8;
+            case 13: return _field[1] == 2 && _field[5] == 4 && _field[7] == 8 && _field[3] == 6;
+            case 20: return _field[0] == 1 && _field[4] == 2 && _field[8] == 3;
+            case 21: return _field[6] == 1 && _field[4] == 2 && _field[2] == 3;
+            case 22: return _field[0] == 3 && _field[4] == 2 && _field[8] == 1;
+            case 23: return _field[6] == 3 && _field[4] == 2 && _field[2] == 1;
+            case 30: return _field[0] == 1 && _field[1] == 2 && _field[2] == 3 && _field[4] == 4 && _field[7] == 5;
+            case 31: return _field[0] == 1 && _field[3] == 2 && _field[6] == 3 && _field[4] == 4 && _field[5] == 5;
+            case 32: return _field[6] == 1 && _field[7] == 2 && _field[8] == 3 && _field[4] == 4 && _field[1] == 5;
+            case 33: return _field[2] == 1 && _field[5] == 2 && _field[8] == 3 && _field[4] == 4 && _field[3] == 5;
         }
         return false;
     }
 
     void initArray()
     {
-        for (int i = 0; i <= 8; i++)
+        int permutations;
+        do
         {
-            field[i] = 10;
-        }
-        field[7] = 11;
-
-        bool exists = true;
-        int next = 0;
-
-
-        while (oddPermutation())
-        {
-
-            for (int i = 0; i <= 8; i++)
+            Debug.Log("Shuffling buttons...");
+            _field = Enumerable.Range(0, 9).ToArray();
+            for (int j = _field.Length; j >= 1; j--)
             {
-                field[i] = 10;
-            }
-            field[7] = 11;
-
-            for (int i = 0; i <= 8; i++)
-            {
-                exists = true;
-
-                int count = 0;
-
-                while (exists == true)
+                int item = Rnd.Range(0, j);
+                if (item < j - 1)
                 {
-                    count++;
-                    exists = false;
-                    next = Rnd.Range(0, 9);
-                    Debug.Log("next: " + next);
-                    for (int j = 0; j <= 8; j++)
-                    {
-                        if (field[j] == next)
-                            exists = true;
-                    }
-                    if (count > 100)
-                    {
-                        Debug.Log("abort while");
-                        break;
-                    }
+                    var t = _field[item];
+                    _field[item] = _field[j - 1];
+                    _field[j - 1] = t;
                 }
-                field[i] = next;
             }
-        }
-        Debug.Log("Field: " + field[0] + field[1] + field[2] + field[3] + field[4] + field[5] + field[6] + field[7] + field[8]);
-    }
 
-    bool oddPermutation()
-    {
-        int permutations = 0;
-        for (int i = 0; i <= 8; i++)
-        {
-            for (int j = 0; j < i; j++)
-            {
-                if (field[i] != 0 && field[j] != 0 && (field[i] < field[j]))
-                    permutations++;
-            }
+            permutations = 0;
+            for (int i = 0; i <= 8; i++)
+                for (int j = 0; j < i; j++)
+                    if (_field[i] != 0 && _field[j] != 0 && (_field[i] < _field[j]))
+                        permutations++;
         }
-        Debug.Log("Permutations: " + permutations);
-        return (permutations % 2) != 0;
+        while (permutations % 2 != 0);
+        Debug.Log("Field: " + _field[0] + _field[1] + _field[2] + _field[3] + _field[4] + _field[5] + _field[6] + _field[7] + _field[8]);
     }
-
-    bool intArrayEquals(int[] a, int[] b)
-    {
-        if (a.Length == b.Length)
-        {
-            for (int i = 0; i < a.Length; i++)
-            {
-                if (a[i] != b[i])
-                    return false;
-            }
-            return true;
-        }
-        return false;
-    }
-
 
     void OnPress(int position)
     {
-        int empty = Array.IndexOf(field, 0);
-        int button = field[position] - 1;
+        if (!_isActivated)
+            return;
+        int empty = Array.IndexOf(_field, 0);
+        int button = _field[position] - 1;
         if (Math.Abs(position % 3 - empty % 3) + Math.Abs(position / 3 - empty / 3) == 1)
         {
-            field[position] = 0;
-            field[empty] = button + 1;
-            var dif = visibleButtons[button].gameObject.transform.position - emptyField.transform.position;
-            visibleButtons[button].gameObject.transform.Translate(-dif, Space.World);
-            emptyField.transform.Translate(dif, Space.World);
+            _field[position] = 0;
+            _field[empty] = button + 1;
+            var dif = ButtonObjects[button].gameObject.transform.position - EmptyField.transform.position;
+            ButtonObjects[button].gameObject.transform.Translate(-dif, Space.World);
+            EmptyField.transform.Translate(dif, Space.World);
 
             // Check for strike or pass
-            if (inDanger)
+            if (_isInDanger)
             {
-                if (field[knight] == 0)
+                if (_field[_knightPos] == 0)
                 {
-                    inDanger = false;
+                    _isInDanger = false;
                     Debug.Log("Found the knight.");
                 }
-                if (field[skull] == 0)
+                if (_field[_skullPos] == 0)
                     GetComponent<KMBombModule>().HandleStrike();
             }
             else if (checkWin())
